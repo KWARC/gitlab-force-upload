fb5e599bf8e05a8eee2673a91ffb38e421c789f0
Tom Wiesing <tkw01536@gmail.com>
2019-02-15 14:06:26 +0100

    Add argument parsing

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("to be implemented")
+}
+
+var verbose bool
+var gitlabURL string
+var authToken string
+var folder string
+var dest string
+
+func init() {
+	flag.BoolVar(&verbose, "v", false, "Log more verbose")
+	flag.StringVar(&gitlabURL, "url", "https://gitlab.com", "GitLab URL to connect to")
+	flag.StringVar(&authToken, "token", "", "Token for GitLab (required)")
+	flag.StringVar(&folder, "folder", "", "Folder to upload to GitLab (required)")
+	flag.StringVar(&dest, "dest", "", "Destionation repository (required)")
+	flag.Parse()
+
+	if authToken == "" {
+		fmt.Println("Missing -token argument")
+		os.Exit(1)
+	}
+
+	// TODO: Check that folder exists
+	if folder == "" {
+		fmt.Println("Missing -folder argument")
+		os.Exit(1)
+	}
+
+	if dest == "" {
+		fmt.Println("Missing -dest argument")
+		os.Exit(1)
+	}
 }
